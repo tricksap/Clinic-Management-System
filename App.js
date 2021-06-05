d80388fd52250d386708639b97c5f7404bd9be84
Patrick <tricksap@gmail.com>
2021-06-05 18:49:28 +0800

    make the row clickable and dynamic

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ const app = express();
 const patientSchema = {
   name: String,
   gender: String,
+  birthdate: Date,
   address: String,
   history: [{ Date: Date, Diagnosis: String }],
 };
@@ -22,7 +23,7 @@ const patientSchema = {
 const Patient = mongoose.model("patient", patientSchema);
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static("public/"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -34,10 +35,15 @@ app.get("/home", function (req, res) {
   res.render("index");
 });
 app.get("/patients", function (req, res) {
-  res.render("tables");
+  Patient.find({}, function (err, found) {
+    res.render("tables", { found: found });
+  });
 });
-app.get("/details", function (req, res) {
-  res.render("details");
+app.get("/details/:id", function (req, res) {
+  const requestedpatient = req.params.id;
+  Patient.findOne({ _id: requestedpatient }, function (err, patient) {
+    res.render("details", { patient: patient });
+  });
 });
 app.get("/register-patient", function (req, res) {
   res.render("NewPatient");
@@ -46,11 +52,17 @@ app.get("/register-patient", function (req, res) {
 app.post("/new-patient", function (req, res) {
   const patient = new Patient({
     name: req.body.name,
+    birthdate: req.body.birthdate,
     gender: req.body.gender,
     address: req.body.address,
   });
   patient.save();
   res.redirect("/home");
+});
+
+app.post("/diagnosis", function (req, res) {
+  Patient.updateOne({ _id: req.body.id }, { Diagnosis: req.body.diagnosis });
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
