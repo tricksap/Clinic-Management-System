77411e0045cea4766e93622e6c943db637b2bbb5
Patrick <tricksap@gmail.com>
2021-06-20 15:37:42 +0800

    add autheticate to access website

--- App.js
+++ App.js
@@ -125,219 +125,291 @@ app.get("/home", function (req, res) {
   }
 });
 app.get("/patients", function (req, res) {
-  Patient.find({}, function (err, found) {
-    res.render("tables", { found: found });
-  });
+  if (req.isAuthenticated()) {
+    Patient.find({}, function (err, found) {
+      res.render("tables", { found: found });
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 app.get("/patients/:id", function (req, res) {
-  const requestedpatient = req.params.id;
-  Patient.findOne({ _id: requestedpatient }, function (err, patient) {
-    let birthdate = moment(patient.birthdate);
-    birthdate = birthdate.format("DD/MM/YYYY");
-    res.render("details", { patient: patient, date: birthdate });
-  });
+  if (req.isAuthenticated()) {
+    const requestedpatient = req.params.id;
+    Patient.findOne({ _id: requestedpatient }, function (err, patient) {
+      let birthdate = moment(patient.birthdate);
+      birthdate = birthdate.format("DD/MM/YYYY");
+      res.render("details", { patient: patient, date: birthdate });
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.delete("/patients/:id", function (req, res) {
-  console.log(req.params.id);
-  Patient.deleteOne({ _id: req.params.id }, function (error, success) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(success);
-    }
-  });
-  res.redirect("/patients");
-});
-
-app.get("/patient/edit/:id", function (req, res) {
-  Patient.findOne({ _id: req.params.id }, function (err, patient) {
-    let birthdate = moment(patient.birthdate);
-    birthdate = birthdate.format("YYYY-MM-DD");
-    res.render("patient-edit", { patient: patient, birthdate: birthdate });
-  });
-});
-
-app.put("/patient/edit/:id", function (req, res) {
-  console.log("asdsadsad");
-  Patient.updateOne(
-    { _id: req.params.id },
-    {
-      name: req.body.name,
-      birthdate: req.body.birthdate,
-      gender: req.body.gender,
-      address: req.body.address,
-    },
-    function (error, success) {
+  if (req.isAuthenticated()) {
+    Patient.deleteOne({ _id: req.params.id }, function (error, success) {
       if (error) {
         console.log(error);
       } else {
         console.log(success);
       }
-    }
-  );
-  res.redirect("/patients");
+    });
+    res.redirect("/patients");
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.get("/patient/edit/:id", function (req, res) {
+  if (req.isAuthenticated()) {
+    Patient.findOne({ _id: req.params.id }, function (err, patient) {
+      let birthdate = moment(patient.birthdate);
+      birthdate = birthdate.format("YYYY-MM-DD");
+      res.render("patient-edit", { patient: patient, birthdate: birthdate });
+    });
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.put("/patient/edit/:id", function (req, res) {
+  if (req.isAuthenticated()) {
+    Patient.updateOne(
+      { _id: req.params.id },
+      {
+        name: req.body.name,
+        birthdate: req.body.birthdate,
+        gender: req.body.gender,
+        address: req.body.address,
+      },
+      function (error, success) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(success);
+        }
+      }
+    );
+    res.redirect("/patients");
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.get("/create", function (req, res) {
-  res.render("NewPatient");
+  if (req.isAuthenticated()) {
+    res.render("NewPatient");
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.post("/create", function (req, res) {
-  const patient = new Patient({
-    name: req.body.name,
-    birthdate: req.body.birthdate,
-    gender: req.body.gender,
-    address: req.body.address,
-  });
-  patient.save();
-  res.redirect("/patients");
+  if (req.isAuthenticated()) {
+    const patient = new Patient({
+      name: req.body.name,
+      birthdate: req.body.birthdate,
+      gender: req.body.gender,
+      address: req.body.address,
+    });
+    patient.save();
+    res.redirect("/patients");
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.post("/diagnosis", function (req, res) {
-  const diagnosis = new Diagnosis({
-    date: moment().format("YYYY-MM-DD"),
-    diagnosis: req.body.diagnosis,
-  });
-  diagnosis.save();
-  Patient.updateOne(
-    { _id: req.body.id },
-    { $push: { history: diagnosis } },
-    function (err, success) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(success);
+  if (req.isAuthenticated()) {
+    const diagnosis = new Diagnosis({
+      date: moment().format("YYYY-MM-DD"),
+      diagnosis: req.body.diagnosis,
+    });
+    diagnosis.save();
+    Patient.updateOne(
+      { _id: req.body.id },
+      { $push: { history: diagnosis } },
+      function (err, success) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(success);
+        }
       }
-    }
-  );
-  res.redirect("/patients");
+    );
+    res.redirect("/patients");
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.get("/diagnosis/edit/:patient_id/:diagnosis_id", function (req, res) {
-  Diagnosis.findOne({ _id: req.params.diagnosis_id }, function (err, result) {
-    let date = moment(result.date);
-    date = date.format("YYYY-MM-DD");
-    res.render("diagnosis-edit", {
-      result: result,
-      date: date,
-      patient_id: req.params.patient_id,
+  if (req.isAuthenticated()) {
+    Diagnosis.findOne({ _id: req.params.diagnosis_id }, function (err, result) {
+      let date = moment(result.date);
+      date = date.format("YYYY-MM-DD");
+      res.render("diagnosis-edit", {
+        result: result,
+        date: date,
+        patient_id: req.params.patient_id,
+      });
     });
-  });
+  } else {
+    res.redirect("/");
+  }
 });
 app.put("/diagnosis/edit/:patient_id/:diagnosis_id", function (req, res) {
-  Diagnosis.updateOne(
-    { _id: req.params.diagnosis_id },
-    {
-      date: req.body.date,
-      diagnosis: req.body.diagnosis,
-    },
-    function (err) {
-      if (err) {
-        console.log(err);
-      }
-    }
-  );
-  Patient.findOneAndUpdate(
-    { _id: req.params.patient_id, "history._id": req.params.diagnosis_id },
-    {
-      $set: {
-        "history.$.date": req.body.date,
-        "history.$.diagnosis": req.body.diagnosis,
+  if (req.isAuthenticated()) {
+    Diagnosis.updateOne(
+      { _id: req.params.diagnosis_id },
+      {
+        date: req.body.date,
+        diagnosis: req.body.diagnosis,
       },
-    },
-    function (err, result) {
-      if (err) {
-        console.log(err);
+      function (err) {
+        if (err) {
+          console.log(err);
+        }
       }
-    }
-  );
-  res.redirect("/patients");
+    );
+    Patient.findOneAndUpdate(
+      { _id: req.params.patient_id, "history._id": req.params.diagnosis_id },
+      {
+        $set: {
+          "history.$.date": req.body.date,
+          "history.$.diagnosis": req.body.diagnosis,
+        },
+      },
+      function (err, result) {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+    res.redirect("/patients");
+  } else {
+    res.redirect("/");
+  }
 });
 app.delete("/diagnosis/delete/:patient_id/:diagnosis_id", function (req, res) {
-  Patient.findByIdAndUpdate(
-    req.params.patient_id,
-    {
-      $pull: {
-        history: { _id: req.params.diagnosis_id },
+  if (req.isAuthenticated()) {
+    Patient.findByIdAndUpdate(
+      req.params.patient_id,
+      {
+        $pull: {
+          history: { _id: req.params.diagnosis_id },
+        },
       },
-    },
-    function (err, result) {
-      if (!err) {
-        console.log(result);
+      function (err, result) {
+        if (!err) {
+          console.log(result);
+        }
       }
-    }
-  );
+    );
 
-  Diagnosis.deleteOne({ _id: req.params.diagnosis_id }, function (err, result) {
-    if (!err) {
-      console.log(result);
-    }
-  });
-  res.redirect("/patients");
+    Diagnosis.deleteOne(
+      { _id: req.params.diagnosis_id },
+      function (err, result) {
+        if (!err) {
+          console.log(result);
+        }
+      }
+    );
+    res.redirect("/patients");
+  } else {
+    res.redirect("/");
+  }
 });
 
 // appointment
 app.get("/appointment", function (req, res) {
-  Appointment.find({}, function (err, result) {
-    res.render("appointment/list", { result: result });
-  });
+  if (req.isAuthenticated()) {
+    Appointment.find({}, function (err, result) {
+      res.render("appointment/list", { result: result });
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 app.post("/appointment", function (req, res) {
-  const appointment = new Appointment({
-    title: req.body.title,
-    start: req.body.startDate + " " + req.body.startTime,
-    end: req.body.endDate + " " + req.body.endTime,
-  });
-  appointment.save();
-  res.redirect("/appointment");
+  if (req.isAuthenticated()) {
+    const appointment = new Appointment({
+      title: req.body.title,
+      start: req.body.startDate + " " + req.body.startTime,
+      end: req.body.endDate + " " + req.body.endTime,
+    });
+    appointment.save();
+    res.redirect("/appointment");
+  } else {
+    res.redirect("/");
+  }
 });
 
 // medicine
 app.get("/medicine", function (req, res) {
-  Medicine.find({}, function (err, result) {
-    res.render("medicine/table", { result: result });
-  });
+  if (req.isAuthenticated()) {
+    Medicine.find({}, function (err, result) {
+      res.render("medicine/table", { result: result });
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.put("/medicine", function (req, res) {
-  console.log(req.body);
-  Medicine.updateOne(
-    { name: req.body.name },
-    {
-      type: req.body.type,
-      description: req.body.description,
-      stocks: req.body.stocks,
-    },
-    function (err, result) {
+  if (req.isAuthenticated()) {
+    Medicine.updateOne(
+      { name: req.body.name },
+      {
+        type: req.body.type,
+        description: req.body.description,
+        stocks: req.body.stocks,
+      },
+      function (err, result) {
+        if (!err) {
+          console.log(result);
+        }
+      }
+    );
+    res.redirect("/medicine");
+  } else {
+    res.redirect("/");
+  }
+});
+app.delete("/medicine", function (req, res) {
+  if (req.isAuthenticated()) {
+    Medicine.deleteOne({ name: req.body.delete }, function (err, result) {
       if (!err) {
         console.log(result);
       }
-    }
-  );
-  res.redirect("/medicine");
-});
-app.delete("/medicine", function (req, res) {
-  Medicine.deleteOne({ name: req.body.delete }, function (err, result) {
-    if (!err) {
-      console.log(result);
-    }
-  });
-  res.redirect("/medicine");
+    });
+    res.redirect("/medicine");
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.get("/medicine/new", function (req, res) {
-  res.render("medicine/new");
+  if (req.isAuthenticated()) {
+    res.render("medicine/new");
+  } else {
+    res.redirect("/");
+  }
 });
 app.post("/medicine/new", function (req, res) {
-  const medicine = new Medicine({
-    name: req.body.name,
-    type: req.body.type,
-    description: req.body.description,
-    stocks: req.body.stocks,
-  });
-  medicine.save();
-  res.redirect("/medicine");
+  if (req.isAuthenticated()) {
+    const medicine = new Medicine({
+      name: req.body.name,
+      type: req.body.type,
+      description: req.body.description,
+      stocks: req.body.stocks,
+    });
+    medicine.save();
+    res.redirect("/medicine");
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.get("*", function (req, res) {
